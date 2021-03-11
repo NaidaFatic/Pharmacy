25392dbe6d7c52aa0b4a047a0abef5dc1820cbd4
NaidaFatic <faticnaida@gmail.com>
2021-03-11 22:27:51 +0100

    insert query

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -5,13 +5,25 @@ require_once dirname(__FILE__). "/BaseDao.class.php";
 class UserDao extends BaseDao{
 
   public function get_user_by_name($name){
-
     return $this->query_unique("SELECT * FROM users WHERE name = :name", ["name" => $name]);
-
-  }
+}
 
   public function get_user_by_id($id){
     return $this->query_unique("SELECT * FROM users WHERE id= :id", ["id" => $id]);
+  }
+
+  public function add_user($user){
+    $insert ="";
+
+    $sql = "INSERT INTO users (name, surname) VALUES (:name, :surname)";
+    $stmt= $this->connection->prepare($sql);
+    $stmt->execute($user);
+    $user["id"]= $this->connection->lastInsertId();
+    return $user;
+  }
+
+  public function update_user($id, $update){
+
   }
 
 }
