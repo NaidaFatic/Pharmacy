af1f75b8612ac3f58b7560d1637351eb077a62d6
NaidaFatic <faticnaida@gmail.com>
2021-04-10 18:13:34 +0200

    config file clean

--- api/clients/SMTPClient.class.php
+++ api/clients/SMTPClient.class.php
@@ -8,9 +8,9 @@ class SMTPClient{
 
   public function __construct(){
     // Create the Transport
-    $transport = (new Swift_SmtpTransport(Config::SMTP_HOST,Config::SMTP_PORT, 'tls'))
-      ->setUsername(Config::SMTP_USER)
-      ->setPassword(Config::SMTP_PASSWORD)
+    $transport = (new Swift_SmtpTransport(Config::SMTP_HOST(),Config::SMTP_PORT(), 'tls'))
+      ->setUsername(Config::SMTP_USER())
+      ->setPassword(Config::SMTP_PASSWORD())
     ;
 
     // Create the Mailer using your created Transport
